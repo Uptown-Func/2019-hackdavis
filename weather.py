abeb98dde9490803d631ffa8245fd15e14203dd7
Armand Agopian <armandagopian457@gmail.com>
2019-02-09 21:06:14 -0800

    Update to match juyter

--- weather.py
+++ weather.py
@@ -1,30 +1,49 @@
-import csv
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+# from sklearn.metrics import mean_squared_error
+# from sklearn.preprocessing import LabelEncoder
+# from sklearn.preprocessing import MinMaxScaler
+from pandas import concat
+from pandas import DataFrame
+from numpy import concatenate
+from math import sqrt
+from matplotlib import pyplot
+from pandas import read_csv
 import tensorflow.keras as keras
 import tensorflow as tf
 print(tf.__version__)
-# initialize data storage
-data = {}
-data['date'] = []
-data['temp'] = []
-data['high'] = []
-data['low'] = []
-data['condition'] = []
 
-conditions = []
-# import data to data
-with open('weather.csv', mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
+# import data
+
+dataset = read_csv('weather.csv')
+dataset
+values = dataset.values
+
+pyplot.figure()
+pyplot.plot(values[:, 1])
+pyplot.title(dataset.columns[1])
+
+pyplot.show()
+
+
+def series_to_supervised(data, n_in=1, n_out=1):
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = DataFrame(data)
+    cols, names = list(), list()
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
         else:
-            data['date'].append(row['date'])
-            data['temp'].append(row['temp'])
-            data['high'].append(row['high'])
-            data['low'].append(row['low'])
-            if row['condition'] not in conditions:
-                conditions.append(row['condition'])
-            data['condition'].append(conditions.index(row['condition']))
-        line_count += 1
-    print(f'Processed {line_count - 1} entries.')
+            names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
+    agg = concat(cols, axis=1)
+    agg.columns = names
+    agg.dropna(inplace=True)
+    return agg
+
+
+print(series_to_supervised([x for x in range(10)], 3))
