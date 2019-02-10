d45a97f1c035afe755b6a39ce979965f8d0d111a
Nicole Zhu <n.zhu5646@berkeley.edu>
2019-02-09 17:41:18 -0800

    now we can get body of messages received

--- server.js
+++ server.js
@@ -5,19 +5,29 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
 
 const app = express();
+var twl, user;
+var prev = [];
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
 
-  twiml.message('The Robots are coming! Head for the hills!');
+  prev.push(req.body.Body);
+  twl = 'The Robots are coming! Head for the hills!';
+  prev.push(twl);
+
+  twiml.message(twl);
   console.log(req.body.Body);
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 });
 
+app.get('/', (req, res) => {
+  user = prev.join('\n')
+  res.send(user);
+});
 // http.createServer(app).listen(1337, () => {
 //   console.log('Express server listening on port 1337');
 // });
