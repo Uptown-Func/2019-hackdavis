658699399b1f03017c7a6bf9063f98a17d35940b
Armand Agopian <armandagopian457@gmail.com>
2019-02-09 18:54:12 -0800

    update gitignore

---
+++ weather.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+print(tf.__version__)
+import tensorflow.keras as keras
+# initialize data storage
+data = {}
+data['date'] = []
+data['temp'] = []
+data['high'] = []
+data['low'] = []
+data['condition'] = []
+
+conditions = []
+# import data to data
+import csv
+with open('weather.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+        else:
+            data['date'].append(row['date'])
+            data['temp'].append(row['temp'])
+            data['high'].append(row['high'])
+            data['low'].append(row['low'])
+            if row['condition'] not in conditions:
+                conditions.append(row['condition'])
+            data['condition'].append(conditions.index(row['condition']))
+        line_count += 1
+    print(f'Processed {line_count - 1} entries.')
